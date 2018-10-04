a9db4a362f4ea55d62ac85908cc1259321738b2c
Muhammed Nurullah Torun <muhamednurullahtorun@gmail.com>
2018-10-04 16:50:59 +0300

    Kosullu operatorler dosyası ile kursa devam ediyorum

--- emty.py
+++ emty.py
@@ -1,2 +1,8 @@
 # -*- coding: utf-8 -*-
 
+sayi = int(input("sayı "))
+
+if sayi > 5:
+    if sayi%2==0:
+        print("doğru")
+        
---
+++ kosullu_oparatorler.py
@@ -0,0 +1,58 @@
+###########################################
+# Author:Dessasin                         #
+# E-mail:muhammednurullahtorun@gmail.com  #
+# github.com/dessasin                     #
+# gitlab.com/dessasin                     #
+# Hakkında:                               #
+###########################################
+
+#%% Bool operatorler kullanımı
+
+# and operatörü
+
+sayi = int(input("sayı "))
+if sayi > 5:
+    if sayi%2==0:           # birinci kullanım biçimi
+        print("doğru")
+
+if sayi >5 and sayi %2 ==0:
+    print("doğru")           # düzenlenmiş ve olması gereken kullanım
+    
+"""
+bu fonksiyonda sayı beşten büyük ve 2 ye bölümünden kalan 0 ise doğru yaz komutunu veriyoruz
+
+"""
+
+# or operatoru
+
+#
+#if sayi>=5:
+#    print("doğru") # birinci kullanım biçimi
+#    
+#
+#if sayi > 5 or sayi ==5:
+#    print("doğru") # düzenlenmiş kullanım 
+
+"""
+bu fonksiyon ile sayı 5 den büyük "yada" eşitse
+""" 
+
+x=int(input("X gir : "))
+y=int(input("Y gir : "))
+
+if x==4 or y== 4 :
+    print("doğru")
+else:
+     print("en az biri 4 olmalı") 
+     
+
+# not operatörü
+
+x=input("X gir : ")
+y=input("Y gir : ")
+
+if bool (x):                #not operatöründen hiçbir şey anlamadım kilit
+    print("yanlış")
+if not bool(x):
+    print("doğru")
+    
