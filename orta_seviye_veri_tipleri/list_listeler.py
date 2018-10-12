c712342853d3816d9989c583a870750fccf55ff2
Muhammed Nurullah Torun <muhammednurullahtorun@gmail.com>
2018-10-12 16:42:13 +0300

    bazı değişiklikler

---
+++ orta_seviye_veri_tipleri/list_listeler.py
@@ -0,0 +1,23 @@
+###########################################
+# Author:Dessasin                         #
+# E-mail:muhammednurullahtorun@gmail.com  #
+# github.com/dessasin                     #
+# gitlab.com/dessasin                     #
+# Hakkında:                               #
+###########################################
+
+listem=[1,2,3,4,5,6,7,8,9,0] # temel liste belirleme yöntemi
+
+
+# listeler C# da kullandığım diziler ile aynı
+
+# listeye veri ekleme
+
+eklenecek="11"
+
+listem+=[eklenecek]
+
+print(listem)
+
+
+#
