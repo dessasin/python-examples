ef66ccfdc19ba3d366f87f1edf8a5861aead253c
Muhammed Nurullah Torun <muhammednurullahtorun@gmail.com>
2018-10-12 18:09:48 +0300

    sözlük veri tipi bitti

--- orta_seviye_veri_tipleri/dict_sozlukler.py
+++ orta_seviye_veri_tipleri/dict_sozlukler.py
@@ -6,3 +6,28 @@
 # Hakkında:                               #
 ###########################################
 
+"""
+sozluk=dict()# sözlük tanımlamalar
+sozluk2={}
+"""
+
+sozluk={"home":"ev","book":"kitap","pen":"kalem"}
+
+print(sozluk["home"])# çıktısı ev
+print(sozluk["book"])#çıktısı kitap
+
+
+# oyunsal sözlük uygulaması
+
+karakter={"ad":"dessas","can":100,"guc":25}
+
+karakter2={"ad":"dessasin","can":100,"guc":35}
+
+
+def vur():
+    karakter2["can"]=karakter2["can"]-karakter["guc"]
+    karakter["can"]=karakter["can"]-karakter2["guc"]
+    print(karakter["can"])
+    print(karakter2["can"])
+
+vur()# hantal bir kod kendim yazdım ancak çalışıyor o zaman ellemeyelim xD
