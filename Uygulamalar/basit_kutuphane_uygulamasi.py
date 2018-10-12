7e901a19275c32ca09486f53433f1db68d80912f
Muhammed Nurullah Torun <muhammednurullahtorun@gmail.com>
2018-10-12 16:59:14 +0300

    basit kütüphane uygulamasi ilk kısım bitti

--- Uygulamalar/basit_kutuphane_uygulamasi.py
+++ Uygulamalar/basit_kutuphane_uygulamasi.py
@@ -6,3 +6,45 @@
 # Hakkında:                               #
 ###########################################
 
+kitapListe=[]
+
+menu="""
+[1] Kitap Ekle
+[2] Kitap Çıkar
+[3] Kitapları Listele
+[q] Çıkış
+"""
+
+def kitapEkle(kitap,liste):
+    liste+=[kitap]
+    print("Kitap Eklendi")
+    input("Ana Menu İçin 'enter' basınız")
+
+def kitapCikar():
+    pass
+
+def listele(liste):
+    for i in liste:
+        print("Kitap adı: {}".format(i))
+    input("Ana Menu İçin 'enter' basınız")
+
+def cik():
+    quit()
+
+
+while True:
+    print(menu)
+    secim=input("Seçiminiz:")
+    if secim=="1":
+        kitapAdi= input("Kitap Adı:")
+        kitapEkle(kitapAdi,kitapListe)
+    
+    elif secim=="2":
+        kitapCikar()
+
+    elif secim=="3":
+        listele(kitapListe)
+    elif secim=="q" or secim=="Q":
+        cik()
+    else:
+        print("hatalı giriş yaptınız")
