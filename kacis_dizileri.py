052d3aff800bf43edb13f77271583cf5d8626733
Muhammed Nurullah Torun <muhammednurullahtorun@gmail.com>
2018-10-12 16:24:26 +0300

    Kaçış dizileri bitti

---
+++ kacis_dizileri.py
@@ -0,0 +1,22 @@
+###########################################
+# Author:Dessasin                         #
+# E-mail:muhammednurullahtorun@gmail.com  #
+# github.com/dessasin                     #
+# gitlab.com/dessasin                     #
+# Hakkında:                               #
+###########################################
+
+# alt satıra atlama
+print("denem\n",end="")
+print("deneme")
+
+# bip sesi çıakaran dizi
+
+print("\a")
+
+
+# sekme (tab dizisi)
+
+print("muhammed")
+print("\tnurullah") # \t tab boşluğu
+print("torun")
