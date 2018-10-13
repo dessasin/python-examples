9e1f80212ac195c538bebdbe918bb642de364d9b
Muhammed Nurullah Torun <muhammednurullahtorun@gmail.com>
2018-10-13 12:52:24 +0300

    tuple bitti

---
+++ orta_seviye_veri_tipleri/tuple_demetler.py
@@ -0,0 +1,15 @@
+###########################################
+# Author:Dessasin                         #
+# E-mail:muhammednurullahtorun@gmail.com  #
+# github.com/dessasin                     #
+# gitlab.com/dessasin                     #
+# Hakkında:                               #
+###########################################
+
+# tuple demet veri tipi
+"""
+ tuplelara ekleme ve çıkarma yapılamaz bu
+ nedenle sürekli değişmeyen standart verilerin tutulduğu noktalarda demet kullanılır
+ """
+demet=("asd0","qweqwe",2) # demet tanımlama
+print(demet)
