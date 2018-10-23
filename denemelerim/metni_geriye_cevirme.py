40d1727ce973038cf6676fd527d35d1abcead59a
Dessasin <dessasin@pop-os.localdomain>
2018-10-23 14:05:03 +0300

    metin geriye çevirme adında bir örnek denedim

---
+++ denemelerim/metni_geriye_cevirme.py
@@ -0,0 +1,16 @@
+###########################################
+# Author:Dessasin                         #
+# E-mail:muhammednurullahtorun@gmail.com  #
+# github.com/dessasin                     #
+# gitlab.com/dessasin                     #
+# Hakkında: notlar içince bulunan örnek   #
+###########################################
+
+#girilen metni geriye çevirip ekrana yazdıracağız
+print("Ters çevirmek istediğiniz metni giriniz ve 'enter' basınız")
+metin=input("Metin: ")
+print(metin[::-1]) # anladığım kadarıyla metin değişkeninin son indeksinden ilk indeksine doğru ekrana yazdırıyoruz doğal olarak terse dönüyor
+
+""" 
+Cevabı internetten bakarak buldum Vigo üstadın pratik bilgilerinden yararlandım
+"""
